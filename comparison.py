3a0f8078660d9e79b2f9ccef5078aa00cbb8c6fc
David Hadka <dmh309@psu.edu>
2015-10-26 22:31:40 -0400

    Clean up comparison example

--- comparison.py
+++ comparison.py
@@ -1,11 +1,11 @@
 from platypus.algorithms import *
-from platypus.problems import DTLZ2
+from platypus.problems import WFG2
 from platypus.operators import GAOperator, SBX, PM
 from multiprocessing import Pool
 import matplotlib.pyplot as plt
 
 # setup the comparison
-problem = DTLZ2()
+problem = WFG2()
 algorithms = [NSGAII(problem, variator=GAOperator(SBX(), PM())),
               NSGAIII(problem, divisions_outer=24, variator=GAOperator(SBX(), PM())),
               CMAES(problem, epsilons=[0.01]),
@@ -25,11 +25,17 @@ map(operator.methodcaller("run", 10000), algorithms)
 def to_points(solutions):
     return [s.objectives[0] for s in solutions], [s.objectives[1] for s in solutions]
 
-fig, axarr = plt.subplots(2, 5)
+fig, axarr = plt.subplots(2, 5, sharex=True, sharey=True)
 
 for i in range(len(algorithms)):
-    axarr[i/5, i%5].scatter(*to_points(algorithms[i].result))
-    axarr[i/5, i%5].set_title(algorithms[i].__class__.__name__)
+    ax = axarr[i/5, i%5]
+    ax.scatter(*to_points(algorithms[i].result))
+    ax.set_title(algorithms[i].__class__.__name__)
+    #ax.set_xlim([0, 1])
+    #ax.set_ylim([0, 1])
 
+fig.text(0.5, 0.04, '$f_1(x)$', ha='center', va='center')
+fig.text(0.04, 0.5, '$f_2(x)$', ha='center', va='center', rotation='vertical')
 
+plt.locator_params(nbins=4)
 plt.show()
