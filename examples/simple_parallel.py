d54689a56353a7b7602164e3e56ac971cb59eb96
David Hadka <dmh309@arl.psu.edu>
2017-02-02 08:50:52 -0500

    Adds example running a single algorithm in parallel

---
+++ examples/simple_parallel.py
@@ -0,0 +1,23 @@
+from platypus.algorithms import NSGAII
+from platypus.problems import DTLZ2
+from platypus.evaluator import ProcessPoolEvaluator
+
+# simulate an computationally expensive problem
+class DTLZ2_Slow(DTLZ2):
+    
+    def evaluate(self, solution):
+        sum(range(1000000))
+        super(DTLZ2_Slow, self).evaluate(solution)
+
+if __name__ == "__main__":
+    # define the problem definition
+    problem = DTLZ2_Slow()
+    
+    # instantiate the optimization algorithm to run in parallel
+    with ProcessPoolEvaluator(4) as evaluator:
+        algorithm = NSGAII(problem, evaluator=evaluator)
+        algorithm.run(10000)
+    
+    # display the results
+    for solution in algorithm.result:
+        print(solution.objectives)
