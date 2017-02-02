f9abeaa718fb836953eec1bbc09670c2db6cee40
David Hadka <dmh309@arl.psu.edu>
2017-02-02 08:45:21 -0500

    Add eNSGAIII

--- platypus/algorithms.py
+++ platypus/algorithms.py
@@ -1482,3 +1482,157 @@ class PESA2(AbstractGeneticAlgorithm):
         else:
             return parent
         
+class PeriodicAction(Algorithm):
+    
+    __metaclass__ = ABCMeta
+    
+    def __init__(self,
+                 algorithm,
+                 frequency = 10000,
+                 by_nfe = True):
+        super(PeriodicAction, self).__init__(algorithm.problem,
+                                             algorithm.evaluator)
+        self.algorithm = algorithm
+        self.frequency = frequency
+        self.by_nfe = by_nfe
+        self.iteration = 0
+        self.last_invocation = 0
+        
+    def step(self):
+        self.algorithm.step()
+        self.iteration += 1
+        self.nfe = self.algorithm.nfe
+        
+        if self.by_nfe:
+            if self.nfe - self.last_invocation >= self.frequency:
+                self.do_action()
+                self.last_invocation = self.nfe
+        else:
+            if self.iteration - self.last_invocation >= self.frequency:
+                self.do_action()
+                self.last_invocation = self.iteration
+                
+    @abstractmethod
+    def do_action(self):
+        raise NotImplementedError("method not implemented")
+        
+    def __getattr__(self, name):
+        return getattr(self.algorithm, name)
+        
+class AdaptiveTimeContinuation(PeriodicAction):
+    
+    def __init__(self,
+                 algorithm,
+                 window_size = 100,
+                 max_window_size = 1000,
+                 population_ratio = 4.0,
+                 min_population_size = 10,
+                 max_population_size = 10000,
+                 mutator = UM(1.0)):
+        super(AdaptiveTimeContinuation, self).__init__(algorithm,
+                                                       frequency = window_size,
+                                                       by_nfe = False)
+        self.window_size = window_size
+        self.max_window_size = max_window_size
+        self.population_ratio = population_ratio
+        self.min_population_size = min_population_size
+        self.max_population_size = max_population_size
+        self.mutator = mutator
+        self.last_restart = 0
+        
+    def check(self):
+        population_size = len(self.algorithm.population)
+        target_size = self.population_ratio * len(self.algorithm.archive)
+        
+        if self.iteration - self.last_restart >= self.max_window_size:
+            return True
+        elif (target_size >= self.min_population_size and
+              target_size <= self.max_population_size and
+              abs(population_size - target_size) > (0.25 * target_size)):
+            return True
+        else:
+            return False
+        
+    def restart(self):
+        archive = self.algorithm.archive
+        population = archive[:]
+            
+        new_size = int(self.population_ratio * len(archive))
+        
+        if new_size < self.min_population_size:
+            new_size = self.min_population_size
+        elif new_size > self.max_population_size:
+            new_size = self.max_population_size
+            
+        offspring = []
+            
+        while len(population) + len(offspring) < new_size:
+            parents = [archive[random.randrange(len(archive))] for _ in range(self.mutator.arity)]
+            offspring.extend(self.mutator.evolve(parents))
+            
+        self.algorithm.evaluate_all(offspring)
+        self.nfe = self.algorithm.nfe
+        
+        population.extend(offspring)
+        archive.extend(offspring)
+        
+        self.last_restart = self.iteration  
+        self.algorithm.population = population
+        self.algorithm.population_size = len(population)
+        
+    def do_action(self):
+        if self.check():
+            self.restart()
+            
+class EpsilonProgressContinuation(AdaptiveTimeContinuation):
+    
+    def __init__(self,
+                 algorithm,
+                 window_size = 100,
+                 max_window_size = 1000,
+                 population_ratio = 4.0,
+                 min_population_size = 10,
+                 max_population_size = 10000,
+                 mutator = UM(1.0)):
+        super(EpsilonProgressContinuation, self).__init__(algorithm,
+                                                          window_size,
+                                                          max_window_size,
+                                                          population_ratio,
+                                                          min_population_size,
+                                                          max_population_size,
+                                                          mutator)
+        self.last_improvements = 0
+        
+    def check(self):
+        result = super(EpsilonProgressContinuation, self).check()
+        
+        if not result:
+            if self.archive.improvements <= self.last_improvements:
+                result = True
+            
+        self.last_improvements = self.archive.improvements
+        return result
+    
+    def restart(self):
+        super(EpsilonProgressContinuation, self).restart()
+        self.last_improvements = self.archive.improvements
+        
+class EpsNSGAII(AdaptiveTimeContinuation):
+    
+    def __init__(self,
+                 problem,
+                 epsilons,
+                 population_size = 100,
+                 generator = RandomGenerator(),
+                 selector = TournamentSelector(2),
+                 variator = None,
+                 **kwargs):
+        super(EpsNSGAII, self).__init__(
+                NSGAII(problem,
+                       population_size,
+                       generator,
+                       selector,
+                       variator,
+                       EpsilonBoxArchive(epsilons),
+                       **kwargs))
+        
