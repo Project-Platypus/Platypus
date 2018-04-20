faf048d5852483eb8ad735c840155b6db161e6b7
David Hadka <dahadka@microsoft.com>
2018-04-20 10:06:06 -0400

    Increment version

--- platypus/__init__.py
+++ platypus/__init__.py
@@ -29,4 +29,4 @@ from .tools import *
 from .types import *
 from .weights import *
 
-__version__ = "1.0.1" # Update setup.py if the version changes!
+__version__ = "1.0.2" # Update setup.py if the version changes!
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class NoseTestCommand(TestCommand):
         nose.run_exit(argv=['nosetests'])
 
 setup(name='Platypus-Opt',
-      version='1.0.1', # Update __init__.py if the version changes!
+      version='1.0.2', # Update __init__.py if the version changes!
       description='Multiobjective optimization in Python',
       author='David Hadka',
       author_email='dhadka@users.noreply.github.com',
