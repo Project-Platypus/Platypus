e85e18a786133ff8da9c414d75050b4de587052d
David Hadka <dhadka@users.noreply.github.com>
2022-11-04 08:23:29 -0500

    Bump version number

--- platypus/__init__.py
+++ platypus/__init__.py
@@ -28,4 +28,4 @@ from .tools import *
 from .types import *
 from .weights import *
 
-__version__ = "1.0.4" # Update setup.py if the version changes!
+__version__ = "1.0.5" # Update setup.py if the version changes!
