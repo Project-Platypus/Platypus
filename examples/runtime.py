7924d18084d4f79e7dc6bfc6146a6b56659413bf
David Hadka <dhadka@users.noreply.github.com>
2024-08-29 00:22:19 +0000

    Use single dict to store data

--- examples/runtime.py
+++ examples/runtime.py
@@ -5,19 +5,19 @@ problem = DTLZ2()
 algorithm = NSGAII(problem)
 
 # Collect the population at each generation.
-runtime = {}
-algorithm.run(10000, callback=lambda a: runtime.update({a.nfe: a.result}))
+results = {}
+algorithm.run(10000, callback=lambda a: results.update({a.nfe: {"population": a.result}}))
 
 # Compute the hypervolume at each generation.
-hypervolume = {}
 ref_set = load_objectives("examples/DTLZ2.2D.pf", problem)
 hyp = Hypervolume(reference_set=ref_set)
 
-for nfe, result in runtime.items():
-    hypervolume[nfe] = hyp.calculate(result)
+for nfe, data in results.items():
+    data["hypervolume"] = hyp.calculate(data["population"])
 
 # Plot the results using matplotlib.
-plt.plot(hypervolume.keys(), hypervolume.values())
+plt.plot(results.keys(),
+         [x["hypervolume"] for x in results.values()])
 plt.xlabel("NFE")
 plt.ylabel("Hypervolume")
 plt.show()
